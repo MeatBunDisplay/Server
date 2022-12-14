d41a7b309244f3ed66a948e27b96a0f1333ab01e
GODofMEGANE <shota20020602@gmail.com>
2022-12-14 15:10:01 +0900

    [add] initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const HTTP_PORT = 80;
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response) => {
+    switch (request.url.split('?')[0]) {
+      case '/index':
+        fs.readFile('./index.html', 'UTF-8', (error, data) => {
+          response.writeHead(200, { 'Content-Type': 'text/html' });
+          response.write(data);
+          response.end();
+        })
+        break;
+      case '/index.js':
+        fs.readFile('./index.js', 'UTF-8', (error, data) => {
+          response.writeHead(200, { 'Content-Type': 'text/plain' });
+          response.write(data);
+          response.end();
+        })
+        break;
+    }
+}).listen(HTTP_PORT);
+console.log(`HTTPサーバをポート${HTTP_PORT}番に立てたよ!!!!!`);
