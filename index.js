8f2dcda162ed02188c1240f151d1497d113772c3
GODofMEGANE <shota20020602@gmail.com>
2022-12-31 23:51:53 +0900

    [add] 肉まんを調理中に出来ます

--- index.js
+++ index.js
@@ -46,16 +46,26 @@ let registered_nikuman = [
         time: 50,
         description: "高いよ",
         place: [6, 7]
+    },
+    {
+        name: "テストまん",
+        price: 200,
+        time: 1,
+        description: "1分で出来ます",
+        place: [8, 9]
     }
 ];
 
 /**
  * @type {[[{
  *  create_time: date,
+ *  finish_time: date,
  *  id: number,
  * }]]}
  */
-let field = [[]];
+let field = [[],[],[],[],[],[],[],[],[],[]];
+
+let nowdate;
 
 class Nikuman{
     /**
@@ -82,11 +92,12 @@ class Nikuman{
 }
 
 function loop(){
-    let date = new Date();
-    date_element.innerText = `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日(${WEEK[date.getDay()]}) ${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
+    nowdate = new Date();
+    updateRender();
 }
 
 function updateRender() {
+    date_element.innerText = `${nowdate.getFullYear()}年${nowdate.getMonth()+1}月${nowdate.getDate()}日(${WEEK[nowdate.getDay()]}) ${nowdate.getHours().toString().padStart(2, "0")}:${nowdate.getMinutes().toString().padStart(2, "0")}:${nowdate.getSeconds().toString().padStart(2, "0")}`;
     let add_items = document.getElementById('add-items');
     let add_item = document.getElementsByClassName('item');
     let len = add_item.length;
@@ -104,20 +115,96 @@ function updateRender() {
         render_name.innerText = registered_nikuman[i].name;
         render_item.appendChild(render_name);
         let number = i;
-        render_item.onclick = () => { clickAddItem(number, new Date()); }
+        render_item.onclick = () => { clickAddItem(number, nowdate); }
         add_items.appendChild(render_item);
     }
-
+    for (let column = 0; column < 10; column++){
+        for (let row = 0; row < 3; row++){
+            let item = document.getElementsByClassName(`column${column} row${row}`)[0];
+            if (row >= field[column].length) {
+                item.innerText = ``;
+                continue;
+            }
+            if (nowdate > field[column][row].finish_time) {
+                item.innerText = `${registered_nikuman[field[column][row].id].name}\n調理済み!`
+            }
+            else {
+                let lefttime = field[column][row].finish_time - nowdate;
+                item.innerText = `${registered_nikuman[field[column][row].id].name}\n${Math.floor(lefttime/1000/60)}:${(Math.floor(lefttime/1000)%60).toString().padStart(2, "0")}`
+            }
+        }
+    }
 }
+
+let cook = 1;
+let cook_date = new Date();
+let add_id = -1;
+let item_count = 0;
 
 function clickAddItem(id, date) {
     console.log("onclick(AddItem)");
+    cook = 1;
+    cook_date = date;
+    add_id = id;
     let panel = document.getElementById('additem-panel');
     let description = document.getElementById('additem-description');
     description.innerText = `商品説明\n${registered_nikuman[id].description}`;
     let time = document.getElementById('additem-time');
     time.innerText = `必要蒸し時間:${registered_nikuman[id].time}分`;
+    let cooking = 0;
+    let cooked = 0;
+    for (let i = 0; i < field.length; i++){
+        for (let j = 0; j < field[i].length; j++){
+            if (field[i][j].id == id) {
+                item_count++;
+                if (nowdate >= field[i][j].finish_time) {
+                    cooked++;
+                }
+                else {
+                    cooking++;
+                }
+            }
+        }
+    }
+    let cooking_count = document.getElementById('additem-cooking');
+    cooking_count.innerText = `蒸し中の個数:${cooking}`;
+    let cooked_count = document.getElementById('additem-cooked');
+    cooked_count.innerText = `蒸しあがった個数:${cooked}`;
+    let cook_count = document.getElementById('additem-cook');
+    cook_count.innerText = `追加する個数:${cook}`;
+    let name = document.getElementById('additem-name');
+    name.innerText = registered_nikuman[id].name;
     panel.style.display = "block";
+}
+
+function changeCookCount(num) {
+    cook = Math.min(Math.max(1, cook + num), registered_nikuman[add_id].place.length * 3 - item_count);
+    let cook_count = document.getElementById('additem-cook');
+    cook_count.innerText = `追加する個数:${cook}`;
+}
+
+function decideAddItem() {
+    let panel = document.getElementById('additem-panel');
+    panel.style.display = "none";
+    let finish = cook_date;
+    finish.setMinutes(finish.getMinutes() + registered_nikuman[add_id].time);
+    let left = cook;
+    for (let j = 0; j < 3; j++){
+        for (let k = 0; k < registered_nikuman[add_id].place.length; k++){
+            if (field[registered_nikuman[add_id].place[k]].length <= j) {
+                field[registered_nikuman[add_id].place[k]][j] = {
+                    id: add_id,
+                    create_time: cook_date,
+                    finish_time: finish
+                }
+                left--;
+                if (left <= 0) {
+                    updateRender();
+                    return;
+                }
+            }
+        }
+    }
 }
 
 function changeTab(tabname){
